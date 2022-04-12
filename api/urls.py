cc4551bbafc6f02b1fba73be3adb3ce2d8b7e130
Harsh Narwariya <61902900+HarshNarwariya@users.noreply.github.com>
2022-04-12 16:23:34 +0530

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -5,10 +5,14 @@ from api.views import SendPasswordResetEmailView, UserChangePasswordView, UserPa
 app_name = 'api'
 
 urlpatterns = [
+    # Auth Urls
     path('register/', UserRegistrationView.as_view(), name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
     path('profile/', UserProfileView.as_view(), name='profile'),
     path('changepassword/', UserChangePasswordView.as_view(), name='changepassword'),
     path('sendpasswordresetemail/', SendPasswordResetEmailView.as_view(), name='sendpasswordresetemail'),
     path('resetpassword/<uid>/<token>/', UserPasswordResetView.as_view(), name='resetpassword'),
+    
+     # adminviews
+    path('getusers/', UsersListView.as_view(), name='getusers'),
 ]
